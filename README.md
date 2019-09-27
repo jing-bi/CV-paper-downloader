6ecb6d8e31ba950ebb6aa13498fb073f69b8fd80
JIng Bi <jbi5@ur.rochester.edu>
2019-09-27 12:30:04 -0400

    finish init

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # CV-paper-downloader
-Web crawler for download paper with keywords from OpenCVF access
+Multiprocess web crawler for download paper with specified keywords from OpenCVF access
+
+## Dependence
+
+Use multiprocess for more flexible multiprocessing.
+
+```bash
+pip install multiprocess tqdm requests beautifulsoup4
+```
+
+## Usage
+
+```python
+# keywords are NOT caps sensitive
+keywords=['egoc','gaze','first']
+# file save path
+save_folder=Path(__file__).parent
+# get a instance of downloader and assign saved format[pdf/markdown]
+downloader=CvSpider(save_folder,keywords)
+downloader.save('md')
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
---
+++ crawler.py
@@ -0,0 +1,73 @@
+import re
+import string
+from pathlib import Path
+
+import requests
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+import multiprocess as mp
+class CvSpider:
+    def __init__(self,save_folder,keywords):
+        self.save_folder=save_folder
+        self.keywords=keywords
+        self.root_url='http://openaccess.thecvf.com/'
+        with open((save_folder / 'title').with_suffix(".md"), 'w') as handler:
+            handler.writelines(f"Keywords: {keywords}\n")
+
+    def _pdf(self,url, title):
+        with open((self.save_folder / title).with_suffix(".pdf"), 'wb') as handler:
+            handler.write(requests.get(url).content)
+    def _md(self,url,title):
+        with open((self.save_folder / 'title').with_suffix(".md"), 'a') as handler:
+            handler.writelines(f"- [{title}]({url})\n")
+    def saveonce(self,arg):
+        name, year, format=arg
+        keywords = sum([[i, i.capitalize()] for i in self.keywords], [])
+        savefunc = {'pdf': self._pdf, 'md': self._md}[format]
+        def parser(element):
+            try:
+                url = self.root_url + \
+                      re.findall(r"(?<=href=\").+?pdf(?=\">pdf)|(?<=href=\').+?pdf(?=\">pdf)", str(element))[0]
+                bibref_strs = re.findall('<div class="bibref">[\s\S]*?</div>', str(element))
+                title = re.findall('\ntitle = {[\s\S]*?<br/>', bibref_strs[0], re.I)[0]
+                title = re.findall('{[\s\S]*?}', title, re.I)[0][1:-1]
+                title = " " + title.translate(str.maketrans(string.punctuation, ' ' * len(string.punctuation)))
+                for word in keywords:
+                    if word in title: title = f"[{word}]" + title
+                if title[0] == '[':
+                    savefunc(url, title + f" [{name}{year}]")
+                    return 1
+                return 0
+            except:
+                return 0
+        link_list = [str(div) for div in BeautifulSoup(requests.get(f'{self.root_url}{name}{year}.py').content, "html.parser").find_all('dd')]
+        num=0
+        t=tqdm(link_list,desc=f'{name}-{year}')
+        for element in t:
+            num+=parser(element)
+            t.set_description(f'{name}{year}|Found:{num}')
+    def save(self,format):
+        conference = [['CVPR', i,format] for i in [x for x in range(2014, 2020)]] + \
+                     [['ICCV', i,format] for i in [x for x in range(2013, 2019, 2)]]
+        with mp.Pool(len(conference)) as pool:
+            pool.imap(self.saveonce, conference)
+            pool.close()
+            pool.join()
+
+if __name__ == "__main__":
+    keywords=['egoc','gaze','first']
+    save_folder=Path(__file__).parent
+
+
+
+
+
+
+
+
+    print(f"Keywords: {keywords}")
+    downloader=CvSpider(save_folder,keywords)
+    downloader.save('md')
+
+
+
